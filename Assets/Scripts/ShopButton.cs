c18c3c3dba99721e3e517cf3f873ecc3af3fea0b
Sam Borg <samuelborg31@gmail.com>
2023-08-23 21:21:40 +0100

    Finished merchant UI

--- Assets/Scripts/ShopButton.cs
+++ Assets/Scripts/ShopButton.cs
@@ -12,6 +12,12 @@ public class ShopButton : MonoBehaviour
     private TextMeshProUGUI nameText, typeText, priceText;
 
     [SerializeField]
+    private GameObject soldOutText;
+
+    private bool isSoldOut;
+    private Color soldOutColor, tooExpensiveColor, defaultColor;
+
+    [SerializeField]
     private string itemName, itemType;
 
     [SerializeField]
@@ -24,18 +30,36 @@ public class ShopButton : MonoBehaviour
         button = gameObject.GetComponent<Button>();
         gameManager = GameObject.FindGameObjectWithTag("GameManager").GetComponent<GameManager>();
         uiManager = GameObject.FindGameObjectWithTag("UI").GetComponent<UIManager>();
+
+        soldOutColor = new Color32(100, 100, 100, 255);
+        tooExpensiveColor = new Color32(225, 0, 0, 255);
+        defaultColor = new Color32(50, 50, 50, 255);
     }
 
+    // Refresh the button's information
     public void RefreshButton() {
+        // Update the text fields
         nameText.text = itemName;
         typeText.text = itemType;
         priceText.text = itemPrice + " Gold";
 
-        if (gameManager.Gold < itemPrice) {
+        if (isSoldOut) {
             button.interactable = false;
+            soldOutText.SetActive(true);
+
+            // Update the disabled color for the button
+            var colorBlock = button.colors;
+            colorBlock.disabledColor = soldOutColor;
+            button.colors = colorBlock;
+            priceText.color = defaultColor;
+        }
+        else if (gameManager.Gold < itemPrice) {
+            button.interactable = false;
+            priceText.color = tooExpensiveColor;
         }
         else {
             button.interactable = true;
+            priceText.color = defaultColor;
         }
     }
 
@@ -44,7 +68,10 @@ public class ShopButton : MonoBehaviour
         // Check if the 
         if (gameManager.Gold >= itemPrice) {
             gameManager.DecreaseGold(itemPrice);
+            isSoldOut = true;
+            // APPLY THE PURCHASE HERE
             uiManager.RefreshShopButtons();
+            uiManager.OnMouseExitButton();
         }
     }
 }
